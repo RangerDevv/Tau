f57453e458f34fb02d194b081bd965688359149c
Rangerdevv <meerabdul2008@gmail.com>
2022-11-26 17:03:20 -0500

    added astro, svelte, and twcss

---
+++ src/components/landing_componenet/nav.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<main>
+    <div class="w-screen h-16 bg-slate-900 flex flex-row gap-x-16 text-white pt-2 text-lg font-sans font-semibold">
+    <div class=" flex justify-end w-screen gap-4 pr-5">
+        <a class=" pt-2">About</a>
+        <a class=" pt-2">Pricing</a>
+        <div class="flex flex-row gap-4 pt-1">
+            <button class=" w-20 h-10 bg-blue-700 text-base rounded-md">Sign Up</button>
+            <button class=" w-20 h-10 bg-green-700 text-base rounded-md">Login</button>
+        </div>
+    </div>
+    </div>
+</main>
---
+++ src/components/test.svelte
@@ -0,0 +1,14 @@
+<script>
+ let count = 0
+ function addNo() {
+    count += 1
+ }
+</script>
+
+<main>
+    <button on:click={addNo} class=" w-20 h-10 bg-blue-700 rounded-md text-white">{count}</button>
+</main>
+
+<style>
+
+</style>
