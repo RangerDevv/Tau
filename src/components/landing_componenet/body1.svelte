7e6ba571fb3f32dab77140ca41fca7bc3ce05a6a
Rangerdevv <meerabdul2008@gmail.com>
2022-11-27 14:39:28 -0500

    added some responsiveness to card

--- src/components/landing_componenet/body1.svelte
+++ src/components/landing_componenet/body1.svelte
@@ -7,7 +7,7 @@ export let Desc;
 </script>
 
 <main>
-   <div class="flex flex-col sm:flex-row justify-center mt-12 gap-80">
+   <div class="flex flex-col sm:flex-row justify-center mt-12 gap-16 sm:gap-80">
     <div class="flex flex-col gap-y-3 pt-12 w-[26rem] 2xl:w-[40rem] order-2 sm:order-1">
         <p class="sm:text-left text-center text-2xl 2xl:text-6xl text-white font-bold">{title}</p>
         <p class="sm:text-left text-center text-lg 2xl:text-3xl text-white">{Desc}</p>
--- src/components/landing_componenet/card.svelte
+++ src/components/landing_componenet/card.svelte
@@ -9,12 +9,12 @@
 <main>
 <div class="flex justify-center pt-12">
     {#if user.loggedIn}
-	<button on:click={toggle} class=" w-96 sm:w-[32rem] h-72 bg-slate-700 text-lg text-white font-sans rounded-xl flex justify-center pt-32">
-		<p class=" text-center w-96"></p>
+	<button on:click={toggle} class=" w-96 sm:w-[32rem] 2xl:w-[40rem] 2xl:h-80  h-72 bg-slate-700 text-lg text-white font-sans rounded-xl flex justify-center pt-16 ">
+		<p class=' text-center text-white w-screen  text-xl 2xl:text-3xl sm:text-2xl font-bold' id='quote'>"When used correctly, flash cards allow students to interact with information in a way that makes it<u class=' underline decoration-red-500 decoration-4'> easier to retain.</u> Flash cards are strategically designed to <u class=' underline decoration-sky-500 decoration-4'>enhance</u> and <u class=' underline decoration-sky-500 decoration-4'>encourage</u> active recall.<br><i class=' underline decoration-yellow-500 decoration-4'>-carsondellosa.com</i></p>
 	</button>
 {:else}
-	<button on:click={toggle} class="w-96 sm:w-[32rem] h-72 bg-slate-700 text-xl text-white font-sans rounded-xl flex justify-center pt-32">
-        <p class="text-center w-96">Click <span class=" underline decoration-orange-600">on the card</span> to know how flashcards can help you <span class=" underline decoration-red-600">learn stuff faster</span></p>
+	<button on:click={toggle} class=" w-96 sm:w-[32rem] 2xl:w-[40rem] 2xl:h-80  h-72 bg-slate-700 text-lg text-white font-sans rounded-xl flex justify-center pt-16 ">
+        <p class="text-center w-96 text-2xl pt-12">Click <span class=" underline decoration-orange-600">on the card</span> to know how flashcards can help you <span class=" underline decoration-red-600">learn stuff faster</span></p>
 	</button>
 {/if}
 </div>
