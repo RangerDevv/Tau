c2cd181f248936bdbc77741da96d8c285f907360
Rangerdevv <meerabdul2008@gmail.com>
2022-11-28 09:44:28 -0500

    fixed typos

--- src/components/landing_componenet/card.svelte
+++ src/components/landing_componenet/card.svelte
@@ -10,7 +10,7 @@
 <div class="flex justify-center pt-12">
     {#if user.loggedIn}
 	<button on:click={toggle} class=" w-96 sm:w-[32rem] 2xl:w-[40rem] 2xl:h-80  h-72 bg-slate-700 text-lg text-white font-sans rounded-xl flex justify-center pt-16 ">
-		<p class=' text-center text-white w-screen  text-xl 2xl:text-3xl sm:text-2xl font-bold' id='quote'>"When used correctly, flash cards allow students to interact with information in a way that makes it<u class=' underline decoration-red-500 decoration-4'> easier to retain.</u> Flash cards are strategically designed to <u class=' underline decoration-sky-500 decoration-4'>enhance</u> and <u class=' underline decoration-sky-500 decoration-4'>encourage</u> active recall.<br><i class=' underline decoration-yellow-500 decoration-4'>-carsondellosa.com</i></p>
+		<p class=' text-center text-white w-screen  text-xl 2xl:text-3xl sm:text-2xl font-bold' id='quote'>"When used correctly, flash cards allow students to interact with information in a way that makes it <u class=' underline decoration-red-500 decoration-4'> easier to retain.</u> Flash cards are strategically designed to <u class=' underline decoration-sky-500 decoration-4'>enhance</u> and <u class=' underline decoration-sky-500 decoration-4'>encourage</u> active recall."<br><i class=' underline decoration-yellow-500 decoration-4'>-carsondellosa.com</i></p>
 	</button>
 {:else}
 	<button on:click={toggle} class=" w-96 sm:w-[32rem] 2xl:w-[40rem] 2xl:h-80  h-72 bg-slate-700 text-lg text-white font-sans rounded-xl flex justify-center pt-16 ">
