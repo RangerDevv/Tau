81c02f0064e39f4df9092efa620f7af600e48c6f
avidcoder123 <umar.tli.edu@gmail.com>
2022-11-29 23:24:20 +0000

    Finish user functions

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -9,6 +9,37 @@ export class User {
         User.usersList.push(this)
     }
 
+    login(email: string, password: string): {
+        successful: boolean, message: string
+    } {
+        let successful = false
+        let userExists = false
+        for(let user of User.usersList) {
+            if(user.email == this.email) {
+                userExists = true
+                if(user.password == this.password) {
+                    successful = true
+                }
+                break
+            }
+        }
+
+        let message = ""
+        if(userExists) {
+            message = "Incorrect Password."
+        } else {
+            message = "That user does not exist."
+        }
+
+        return {
+            successful, message
+        }
+    }
+
+    logout() {
+        //Do supabase logout here
+    }
+
     email: string
     password: string
 
